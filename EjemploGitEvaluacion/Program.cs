0318eeaa242a541b86d4592ce75f9aa347b0783d
josue <josue@DEIVLER575>
2026-02-26 22:59:44 -0500

    Agregar archivos de proyecto.

---
+++ EjemploGitEvaluacion/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace EjemploGitEvaluacion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculadora calc = new Calculadora();
+            int resultado = calc.Sumar(5, 7);
+            Console.WriteLine($"El resultado de la suma es: {resultado}");
+        }
+    }
+
+    public class Calculadora
+    {
+        public int Sumar(int a, int b)
+        {
+            return a + b; // Aquí experimentamos y creamos la solución
+        }
+    }
+}
